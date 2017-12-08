bee216db0e6be8decc3693dcd3bb4b51885c1c29
Rita Radovics <Rita@Ritas-MacBook-Pro-2.local>
2017-12-08 11:43:14 +0100

    After Jonas

--- js/java.js
+++ js/java.js
@@ -1,61 +1,46 @@
-function getData(){
-    fetch("http://zawropati.com/GARN/wp-json/wp/v2/hairdressers")
-    .then(res=>res.json())
-    .then(showHairdressers)
+function getData() {
+    fetch("http://zawropati.com/GARN/wp-json/wp/v2/hairdressers?_embed").then(res => res.json()).then(showHairdressers);
 }
 
-function showHairdressers(data){
+function showHairdressers(data) {
     let list = document.querySelector("#aboutlist");
     let template = document.querySelector("#hairdressersTemplate").content;
-
-    data.forEach (function(theHairdresser){
+    data.forEach(function (theHairdresser) {
         console.log(theHairdresser)
-    let clone = template.cloneNode(true);
+        let clone = template.cloneNode(true);
         let name = clone.querySelector(".name");
         name.textContent = theHairdresser.acf.name;
         let description = clone.querySelector(".description");
-        description.textContent = theHairdresser.acf.description;
-         let image = clone.querySelector(".image");
+        description.innerHTML = theHairdresser.acf.description;
+        let image = clone.querySelector(".image");
+        image.setAttribute('src', theHairdresser._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url);
+        let instagram = clone.querySelector(".instagram");
+        instagram.setAttribute('href',theHairdresser.acf.instagram);
 
-
-    list.appendChild(clone);
-})
-
-
-
+        list.appendChild(clone);
+    })
 }
 
-function getPrices(){
-    fetch("http://zawropati.com/GARN/wp-json/wp/v2/prices")
-    .then(res=>res.json())
-    .then(showPrices)
+function getPrices() {
+    fetch("http://zawropati.com/GARN/wp-json/wp/v2/prices").then(res => res.json()).then(showPrices)
 }
 
-function showPrices(prices){
-
+function showPrices(prices) {
     let pricelist = document.querySelector("#pricelist");
     let pricestemplate = document.querySelector("#pricesTemplate").content;
-
-    data.forEach(function(aPrice){
+    prices.forEach(function (aPrice) {
         console.log(aPrice)
-   let clone = pricestemplate.cloneNode(true);
+        let clone = pricestemplate.cloneNode(true);
         let title = clone.querySelector(".title");
         title.textContent = aPrice.title.rendered;
         let price = clone.querySelector(".price");
         price.textContent = aPrice.acf.price;
-     pricelist.appendChild(clone);
-
-       /*  let name = clone.querySelector(".name");
-        name.textContent = theHairdresser.acf.name;
-        let description = clone.querySelector(".description");
-        description.textContent = theHairdresser.acf.description;
-         let image = clone.querySelector(".image");
-
-
-     */
-})
-
+        pricelist.appendChild(clone);
+    })
 }
-
-
-getData();
+if(document.querySelector("#hairdressersTemplate")){
+    getData();
+}
+if(document.querySelector("#pricesTemplate")){
+ getPrices();
+}
